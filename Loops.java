1c18c5351380abdcd390200f3c40f207459848da
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-07-22 12:48:03 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,3 +1,19 @@
 public class Loops{
+  public void method1(){
+    for (i=0;i>=10;i++){
+      System.out.println("SHREYASH " + i)
+    }
+  }
+   public void method2(){
+     int a=0;
+     while (i>=10){
+       System.out.println("ARGHODE " +a)
+         a+1;
+     }
+     public static void main (String[] args){
+     new Loops().method1();
+      new Loops().method1();
+       
+     }
 // Prepare a For Loop and While  in each Function
 }
